1a11052dbeb961f2de67f12f499e339644962d1e
unknown <mrashes2@gmail.com>
2017-08-17 17:43:37 -0500

    card style work

---
+++ maxTest/cards.js
@@ -0,0 +1,11 @@
+//aiming for 20 adjectives  Have 10 right now
+let adjectives = {
+    adjectives: ["funny", "cute", "sad", "clean", "hyper", "fast", "slow", "creative", "hungry", "fancy"]
+}
+
+
+
+//aiming for 100 nouns
+let nouns = {
+    nouns: ["Richard Nixon", "Cloud Gate", "Rham Emanuel", "The loop", "Museum of Science and Industry", "Helen Keller", "Field Museum", ""]
+}
